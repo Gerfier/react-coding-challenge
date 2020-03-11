f0b739a764ffbb38d4f2936afee2b46d165c34f0
Gerard <gerrardf7@gmail.com>
2020-03-11 15:06:13 -0500

    fix message test suit

--- src/components/__tests__/message.test.js
+++ src/components/__tests__/message.test.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { render, fireEvent, getByRole } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Message from '../message'
 
 describe('message', () => {
   const handleMessage = jest.fn();
   const props = {
     data: {    
-      message: "fkhkjffkgfgskls kdhjhd."
+      message: "fkhkjffkgfgskls kdhjhd.",
+      id: "73897",
+      priority: 2,
     },
     msgClass: { backgroundColor: '#F56236' },
     handleMessage,
@@ -17,15 +19,15 @@ describe('message', () => {
     expect(container).toBeVisible()
   })
 
-  it('displays button text', () => {
+  it('displays button to remove message', () => {
     const { container } = render(<Message {...props}/>)
     const button = container.querySelector('button')
-    expect(button).toHaveTextContent("Clear")
+    expect(button).toBeVisible()
   });
 
   it('handleMessage event handler triggers', () => {
     const { container } = render(<Message {...props}/>)   
-    fireEvent.click(getByRole(container, 'button'));
+    fireEvent.click(container.querySelector('button'));
     expect(handleMessage).toHaveBeenCalledTimes(1)
   });
 })
